a05cc9ad083a5b3e1ce3b9a23c1b672bc9ae2e8c
dwikifadhilah <dwkfadhilah@gmail.com>
2022-09-18 15:53:13 +0700

    Change login testdata

--- src/main/java/testdata/LoginData.java
+++ src/main/java/testdata/LoginData.java
@@ -5,6 +5,6 @@ import lombok.Getter;
 
 @Getter
 public class LoginData {
-		private final String email = "punya.dwiki@gmail.com";
-		private final String password = "punya.dwiki";
+		private final String email = "email@domain.com";
+		private final String password = "password";
 }
